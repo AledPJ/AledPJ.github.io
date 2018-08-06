8840d2031f5d05a4bc48fb63952767f35bc1e113
AledPJ <35375695+AledPJ@users.noreply.github.com>
2018-08-06 15:10:14 +0100

    Complete Website

---
+++ js/javascript.js
@@ -0,0 +1,69 @@
+
+
+
+jQuery(document).ready(function($)
+{
+	$(".MythicalAnimalsSlider").unslider();
+		arrows: true
+});
+
+jQuery(document).ready(function($)
+{
+	$(".AnimalsSlider").unslider();
+		arrows: true
+});
+
+jQuery(document).ready(function($)
+{
+	$(".BronzeAnimalsSlider").unslider();
+		arrows: true
+});
+
+jQuery(document).ready(function($)
+{
+	$(".GlassFlowersSlider").unslider();
+		arrows: true
+});
+
+function MythicalOn() 
+{
+    document.getElementById("MythicalOverlay").style.display = "block";
+}
+
+function MythicalOff() 
+{
+    document.getElementById("MythicalOverlay").style.display = "none";
+}
+
+function AnimalsOn() 
+{
+    document.getElementById("AnimalsOverlay").style.display = "block";
+}
+
+function AnimalsOff() 
+{
+    document.getElementById("AnimalsOverlay").style.display = "none";
+}
+
+function BronzeOn() 
+{
+    document.getElementById("BronzeOverlay").style.display = "block";
+}
+
+function BronzeOff() 
+{
+    document.getElementById("BronzeOverlay").style.display = "none";
+}
+
+function FlowersOn() 
+{
+    document.getElementById("FlowersOverlay").style.display = "block";
+}
+
+function FlowersOff() 
+{
+    document.getElementById("FlowersOverlay").style.display = "none";
+}
+
+
+
